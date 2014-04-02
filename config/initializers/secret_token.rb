2fe1296a22b1b638ccef8a8b6b28f501aa86bd9c
Dung (Don) A. LE <dung.a.le@localhost.localdomain>
2014-04-02 11:37:33 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Myapp::Application.config.secret_key_base = '98d67a0eddcf59106e1450a9f446e0fe879e8d80f33eafb3522e5bf0765fd5ac8fe7aa666fa42bdf9ac62e1ac929bcb27abbac65e852364c2ce54d586a56c9ab'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Myapp::Application.routes.draw do
+  resources :tasks
+  root :to =>'task#index'
+end
